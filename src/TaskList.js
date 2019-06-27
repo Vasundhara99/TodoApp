46492fb5bf380b43b9ae64ed61b9765190c2873f
Vasundhara <vasundhara.myakala@zemosolabs.com>
2019-06-27 14:16:48 +0530

    Changing files

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,59 +1,72 @@
+/* eslint-disable no-console */
 import React from "react";
-import {TextField,Button,List,ListItem,ListItemText} from '@material-ui/core'
-class TaskList extends React.Component
-{
-    constructor()
-    {
+import { TextField, Button, List, ListItem, ListItemText } from '@material-ui/core'
+class TaskList extends React.Component {
+    constructor() {
         super()
-        this.state={
-            taskName:[],value:""
+        this.state = {
+            taskName: [], value: ""
         }
-        this.handleChange=this.handleChange.bind(this)
-        this.handleButton=this.handleButton.bind(this)
-        this.render=this.render.bind(this)
-
+        this.handleChange = this.handleChange.bind(this)
+        this.handleButton = this.handleButton.bind(this)
+        this.render = this.render.bind(this)
+        this.deleteTask = this.deleteTask.bind(this)
+        this.editTask = this.editTask.bind(this)
     }
-    handleChange(event)
-    {
+    handleChange(event) {
         this.setState(
-            {value:event.target.value}
+            { value: event.target.value }
         )
     }
-    handleButton()
-    {
-        if(this.state.value!=="")
-        this.setState(
-            {taskName:[...this.state.taskName,this.state.value]}
-          )
+    handleButton() {
+        if (this.state.value !== "")
+            this.setState(
+                { taskName: [...this.state.taskName, this.state.value] }
+            )
     }
-    render()
-    {
-        return(<form>
+    deleteTask(event) {
+        let array = [...this.state.taskName]
+        let index = array.indexOf(event.currentTarget.id);
+        array.splice(index, 1);
+        this.setState(
+            { taskName: array }
+        )
+    }
+    editTask(event) {
+        let array = [...this.state.taskName]
+        let index = array.indexOf(event.currentTarget.id);
+        let edited = prompt("Edit the task")
+        array[index] = edited
+        this.setState(
+            { taskName: array }
+        )
+    }
+    render() {
+        return (<form><h1>TODO LIST</h1>
             <TextField
-        id="outlined-name"
-        label="Task"
-        onChange={this.handleChange}
-        margin="normal"
-        variant="outlined"
-      />
-      <br></br>
-      <Button variant="contained" color="primary" onClick={this.handleButton}>
-        ADD TASK
+                id="outlined-name"
+                label="Task"
+                onChange={this.handleChange}
+                margin="normal"
+                variant="outlined"
+            />
+            <br></br>
+            <Button variant="contained" color="primary" onClick={this.handleButton}>
+                ADD TASK
       </Button>
-      <List>
-            {this.state.taskName.map(item=>(
-      <ListItem key={item} alignItems="flex-start">
-        <ListItemText>{item}
-        &emsp;<Button variant="contained" size="small">EDIT</Button>
-        &emsp;<Button variant="contained" size="small">DELETE</Button>
-        </ListItemText>
-     
-      </ListItem>
-      ))}
-      </List>
+            <hr />
+            <h1>YOUR TASKS</h1>
+            <List>
+                {this.state.taskName.map(item => (
+                    <ListItem key={item} alignItems="flex-start">
+                        <ListItemText>{item}
+                            &emsp;<Button id={item} variant="contained" size="small" onClick={this.editTask}>EDIT</Button>
+                            &emsp;<Button id={item} variant="contained" size="small" onClick={this.deleteTask}>DELETE</Button>
+                        </ListItemText>
+                    </ListItem>
+                ))}<hr />
+            </List>
         </form>)
     }
 }
-
-
 export default TaskList
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TaskList from "./TaskList"
-    ReactDOM.render(<TaskList />,document.getElementById("root"))
+ReactDOM.render(<TaskList />, document.getElementById("root"))
